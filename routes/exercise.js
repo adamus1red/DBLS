e4d552e55940f58dfe844c1a5b8e74af09331f1c
Adam McGhie <adam@mcghie.eu>
2016-02-23 16:38:03 +0000

    remove api auth

--- routes/exercise.js
+++ routes/exercise.js
@@ -62,42 +62,36 @@ router.post('/:eid', function(req,res,next){
     }
 });
 
-router.post('/:eid/api', function(req,res,next){
-    console.log(req.body);
-    
-    if(req.isAuthenticated()){
-        db.get("SELECT * FROM exercise WHERE id = ?", req.params.eid, function(err, row){
-            if(err){
-                res.setHeader('Content-Type', 'application/json');
-                res.send(err)
-                console.err(err);
-            } else if (typeof row === 'undefined'){
-                res.setHeader('Content-Type', 'application/json');
-                res.send({message: "No such question", error: {status: "QE404",stack: "Yo' no question with that ID exists. Try a different one"}});
-            } else {
-                //console.log();
-                var db2 = new sqlite3.Database(saveOut(req, row.testDB));
-                db2.all(req.body.sql, function(err, userRow){
-                    if(err){
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send({exID: req.params.eid, question : row.question, output: err});
-                    } else if (typeof userRow === 'undefined') {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send({message: "Something went horribly wrong", error: {status: "QE403",stack: "Something hit the fan and blew everywhere."}});
-                    } else {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify(JSON.stringify(userRow)));
-                    }
-                    
-                });
-                fs.unlink(dir + "/"+ req.user._id + req.params.eid + ".db");
-                //res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : row.question, output: req.body.sql});
-            }
-            
-        });
-    } else {
-        res.redirect('/login');
-    }
+router.post('/:eid/api', function(req,res,next){    
+    db.get("SELECT * FROM exercise WHERE id = ?", req.params.eid, function(err, row){
+        if(err){
+            res.setHeader('Content-Type', 'application/json');
+            res.send(err)
+            console.err(err);
+        } else if (typeof row === 'undefined'){
+            res.setHeader('Content-Type', 'application/json');
+            res.send({message: "No such question", error: {status: "QE404",stack: "Yo' no question with that ID exists. Try a different one"}});
+        } else {
+            //console.log();
+            var db2 = new sqlite3.Database(saveOut(req, row.testDB));
+            db2.all(req.body.sql, function(err, userRow){
+                if(err){
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send({exID: req.params.eid, question : row.question, output: err});
+                } else if (typeof userRow === 'undefined') {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send({message: "Something went horribly wrong", error: {status: "QE403",stack: "Something hit the fan and blew everywhere."}});
+                } else {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify(JSON.stringify(userRow)));
+                }
+                
+            });
+            fs.unlink(dir + "/"+ req.user._id + req.params.eid + ".db");
+            //res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : row.question, output: req.body.sql});
+        }
+        
+    });
 });
 
 
