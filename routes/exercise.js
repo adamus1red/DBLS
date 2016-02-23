7446e20c88f2b95280afbd5a738c795c1347f570
Adam McGhie <adamus1red@gmail.com>
2016-02-23 13:21:11 +0000

    attempt fix

--- routes/exercise.js
+++ routes/exercise.js
@@ -36,17 +36,19 @@ router.post('/:eid', function(req,res,next){
             } else if (typeof row === 'undefined'){
                 res.render('error.ejs', {message: "No such question", error: {status: "QE404",stack: "Yo' no question with that ID exists. Try a different one"}});
             } else {
-                var db2 = new sqlite3.Database(row.testDB);
-                db2.get(req.body.sql, function(err, row){
+                console.log(row.testDB);
+                /*var db2 = new sqlite3.Database(row.testDB);
+                db2.get(req.body.sql, function(err, row2){
                     if(err){
                         res.render('exercise.ejs', {exID: req.params.eid, question : row.question, output: err});
-                    } else if (typeof row === 'undefined') {
+                    } else if (typeof row2 === 'undefined') {
                         res.render('error.ejs', {message: "Something went horribly wrong", error: {status: "QE403",stack: "Something hit the fan and blew everywhere."}});
                     } else {
-                        res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : row.question, output: row});
+                        res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : ro w.question, output: row2});
                     }
                     
-                });
+                }); */
+                res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : row.question, output: req.body.sql});
             }
             
         });
