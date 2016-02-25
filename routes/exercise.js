eb9394e28e63484cadbf28a7822750e0b9f325d9
adamus1red <adam@mcghie.eu>
2016-02-25 09:39:38 +0000

    fix UI overlap on index page

--- routes/exercise.js
+++ routes/exercise.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 var util = require('util')
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./exercise.db');
+var db = new sqlite3.Database('./exercise.db', sqlite3.OPEN_READWRITE);
 var fs = require('fs');
 var dir = './tmp';
 
@@ -50,6 +50,9 @@ router.post('/:eid', function(req,res,next){
                         res.render('error.ejs', {message: "Something went horribly wrong", error: {status: "QE403",stack: "Something hit the fan and blew everywhere.", user : req.user}});
                     } else {
                         db2.all(row.testQuery, function(err, correctRow) {
+                            if(err) {
+                                res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : row.question, output: JSON.stringify(err, null, 4), user : req.user, sentValue: req.body.sql, answerState : 3});
+                            }
                             var userAnswer = JSON.stringify(userRow), correctAnswer = JSON.stringify(correctRow);
                             if(userAnswer == correctAnswer) {
                                 res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : row.question, output: JSON.stringify(userRow, null, 4), user : req.user, sentValue: req.body.sql, answerState : 2});
@@ -70,39 +73,6 @@ router.post('/:eid', function(req,res,next){
     }
 });
 
-router.post('/:eid/api', function(req,res,next){    
-    db.get("SELECT * FROM exercise WHERE id = ?", req.params.eid, function(err, row){
-        if(err){
-            res.setHeader('Content-Type', 'application/json');
-            res.send(err)
-            console.err(err);
-        } else if (typeof row === 'undefined'){
-            res.setHeader('Content-Type', 'application/json');
-            res.send({message: "No such question", error: {status: "QE404",stack: "Yo' no question with that ID exists. Try a different one"}});
-        } else {
-            //console.log();
-            var db2 = new sqlite3.Database(saveOut(req, row.testDB));
-            db2.all(req.body.sql, function(err, userRow){
-                if(err){
-                    res.setHeader('Content-Type', 'application/json');
-                    res.send({exID: req.params.eid, question : row.question, output: err});
-                } else if (typeof userRow === 'undefined') {
-                    res.setHeader('Content-Type', 'application/json');
-                    res.send({message: "Something went horribly wrong", error: {status: "QE403",stack: "Something hit the fan and blew everywhere."}});
-                } else {
-                    res.setHeader('Content-Type', 'application/json');
-                    res.send(JSON.stringify(JSON.stringify(userRow)));
-                }
-                
-            });
-            fs.unlink(dir + "/"+ req.user._id + req.params.eid + ".db");
-            //res.render('exercise.ejs', {exID: req.params.eid, title: "Exercise " + req.params.eid, question : row.question, output: req.body.sql});
-        }
-        
-    });
-});
-
-
 module.exports = router;
 
 function saveOut(req, data) {
@@ -117,4 +87,8 @@ function saveOut(req, data) {
         console.log("The file was saved! " +  dir + "/"+ req.user._id + req.params.eid + ".db"  );
     }); 
     return dir + "/"+ req.user._id + req.params.eid + ".db";
+}
+
+function typeOne(req, res, next, row){
+    
 }
