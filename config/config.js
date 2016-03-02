a06749058f4d2bd57b8e62df5054c7b707a4fada
adamus1red <adam@mcghie.eu>
2016-03-02 17:33:16 +0000

    remove config

--- config/config.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'dburl' : 'mongodb://localhost/passport', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'cookiesecret' : "secret"
-    
-};
