46ef4f22346dc294774347af26f9b14d0d1c0ce1
adamus1red <adam@mcghie.eu>
2016-03-10 23:31:08 +0000

    fix not displaying exercises correctly when none have been answered

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,7 @@ router.get('/', loggedIn, function(req, res, next) {
                 res.render('error.ejs', {message: "SQLITE DB error", error: {status: "DB02",stack: err,user : req.user}});
             }
             if(rr == "") {
-                res.render('profile.ejs', {title: "Profile", user : req.user, userAvatarHash: hash, questions: row, qdone: {exID:0}});
+                res.render('profile.ejs', {title: "Profile", user : req.user, userAvatarHash: hash, questions: row, qdone: [{exID:0}]});
             } else {
                 res.render('profile.ejs', {title: "Profile", user : req.user, userAvatarHash: hash, questions: row, qdone: rr});
             }
