0f3e26e1fc07c92cf3e3c2c1955361799e2d3087
adamus1red <adam@mcghie.eu>
2016-03-23 01:35:26 +0000

    seriealize breaks stuff mkay# Please enter the commit message for your changes. Lines starting

--- routes/staff.js
+++ routes/staff.js
@@ -54,49 +54,43 @@ function isAdmin(req, res, next) {
 
 function totalCalcs(req, res, next) {
     var totals = {};
-    db.serialize(function() {
-        db.all("SELECT * FROM exercise", function(err, row){
-            if(err){
-                console.error(err);
-                res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP01",stack: err,user : req.user}});
-            } else {
-            totals.exercises = row;
-            
-                db.all("SELECT * FROM answers", function(err, row) {
-                    if(err){
-                        console.error(err);
-                        res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP02",stack: err,user : req.user}});
-                    } else {
-                        totals.answers = row;
-                        next(totals);
-                    }
-                });
-            }
-        });
-        db.close();
+    db.all("SELECT * FROM exercise", function(err, row){
+        if(err){
+            console.error(err);
+            res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP01",stack: err,user : req.user}});
+        } else {
+        totals.exercises = row;
+        
+            db.all("SELECT * FROM answers", function(err, row) {
+                if(err){
+                    console.error(err);
+                    res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP02",stack: err,user : req.user}});
+                } else {
+                    totals.answers = row;
+                    next(totals);
+                }
+            });
+        }
     });
 }
 
 function exInfo(req, res, next) {
     var data = {};
-    db.serialize(function() {
-        db.get("SELECT * FROM exercise WHERE id = ?", req.params.eid , function(err, row){
+    db.get("SELECT * FROM exercise WHERE id = ?", req.params.eid , function(err, row){
+        if(err){
+            console.error(err);
+            res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP01",stack: err,user : req.user}});
+        } else {
+        data.exercises = row;
+        db.all('SELECT * FROM "answers" WHERE userID = ?1 AND exID = ?2', {1: req.user.id, 2: req.params.eid}, function(err, acheck) {
             if(err){
                 console.error(err);
-                res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP01",stack: err,user : req.user}});
+                res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP02",stack: err,user : req.user}});
             } else {
-            data.exercises = row;
-            db.all('SELECT * FROM "answers" WHERE userID = ?1 AND exID = ?2', {1: req.user.id, 2: req.params.eid}, function(err, acheck) {
-                if(err){
-                    console.error(err);
-                    res.render('error.ejs', {message: "SQLITE DB error", error: {status: "SP02",stack: err,user : req.user}});
-                } else {
-                    data.answers = acheck;
-                    next(data);
-                }
-            });
+                data.answers = acheck;
+                next(data);
             }
         });
-        db.close();
+        }
     });
 }
